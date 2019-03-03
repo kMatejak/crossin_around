adfa78b20786753be10c779536ca2134739881e9
kMatejak <ksmatejak@gmail.com>
2019-03-03 17:27:28 +0100

    clean code and added functions player_moves and next_turn to data_manager.py

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -1,26 +1,37 @@
-X = '+'
-O = 'o'
+from display_in_console import show
+
 EMPTY = ' '
 TIE = 'TIE'
 
 
 def ask_yes_no(question):
-    """Ask a question that you can answer 'yes' or 'no'"""
-    response = None
-    while response not in ('t', 'n', '', 'maciej jest spoko'):
-        response = input(question).lower()
-    return response
+    """
+    Ask a question that you can answer 'yes' or 'no'
+    """
+    answer = False
+    counter = 0
+    
+    while answer not in ('t', 'y', 'n', '', 'maciej jest spoko') and counter < 4:
+        answer = input(question).lower()
+        counter += 1
+    
+    return answer
 
 
 def ask_number(question):
-    """Ask about the number of the selected field"""
-    response = None
-    while response not in SHOTS:
-        response = int(input(question))
-    return response
+    """
+    Ask about the number of the selected field
+    """
+    answer = False
+    counter = 0
+
+    while answer not in range(1, 10) and counter < 4:
+        answer = int(input(question))
+        counter += 1
+    return answer
 
 
-def create_new_empty_board():
+def create_empty_board():
     '''
     :return: dict
     '''
@@ -38,10 +49,26 @@ def create_new_empty_board():
     return new_board 
 
 
-def legal_moves(grid):
-    """Create list of legal moves"""
-    moves = []
-    for field in SHOTS:
-        if grid[field] == EMPTY:
-            moves.append(field)
-    return moves
+def player_moves(turn, board):
+    move = input('Wskaż pole swojego najazdu: ')
+    if move not in ['1','2','3','4','5','6','7','8','9']:
+        show.error_not_valid_type()
+        raise TypeError
+    elif board[move] != EMPTY:
+        show.error_illegal_move()
+        raise TypeError
+    else:
+        board[move] = turn
+        return board
+
+
+def next_turn(turn):
+    if turn == '+':
+        turn = 'o'
+    else:
+        turn = '+'
+    return turn
+
+
+def winner(board):
+    pass
