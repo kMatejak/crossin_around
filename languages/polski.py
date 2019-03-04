7545475e98936f0c181291db83625d3bd84f493e
kMatejak <ksmatejak@gmail.com>
2019-03-04 19:06:07 +0100

    fixed nonvisible textes

--- languages/polski.py
+++ languages/polski.py
@@ -1,5 +1,5 @@
 def show_instruction_1():
-    return '\tSułtan Kefiratu pomny o zniewagę jakiej doznał, wyzwał na poje- \
+    show = print('\tSułtan Kefiratu pomny o zniewagę jakiej doznał, wyzwał na poje- \
         \n\tdynek dzielnego Krzyżowca Ogórkomira. Wciel się w rolę waleczn- \
         \n\tego Krzyżowca lub bezwględnego Sułtana i wgnieć swojego przeci-\
         \n\twnika w piaski pustyni.\
@@ -8,55 +8,71 @@ def show_instruction_1():
         \n\ta Sułtan puszcza toksyczne kółka z fajki wodnej. Ruch odbywa\
         \n\tsię na zmianę poprzez wskazanie numeru pustego pola. \
         \n\n\tZwyciężysz, o ile zajmiesz trzy sąsiadujące ze sobą pola:\
-        \n\tpoziomo, pionowo lub ukośnie.'
+        \n\tpoziomo, pionowo lub ukośnie.')
+    return show
 
 
 def show_instruction_2():
-    return '\n\tTwój przykładowy tryumf może wyglądać tak:'
+    show = print('\n\tTwój przykładowy tryumf może wyglądać tak:')
+    return show
 
 
 def show_instruction_3():
-    return '\tDociśnij dowolnego konia [klawiszem Enter], aby kontynuować...'
+    show = print('\tDociśnij dowolnego konia [klawiszem Enter], aby kontynuować...')
+    return show
 
 
 def show_opening_question():
-    return '\tCzy jesteś Krzyżowcem? '
+    show = print('\tCzy jesteś Krzyżowcem? ')
+    return show
 
 
 def show_player_cheer():
-    return '\t\tNAJEŻDŻAJ!!!'
+    show = print('\t\tNAJEŻDŻAJ!!!')
+    return show
 
 
 def show_next_move_ask():
-    return '\n\tWskaż pole swojego najazdu: '
+    show = print('\n\tWskaż pole swojego najazdu: ')
+    return show
 
 
 def show_sultan_wins_ending():
-    return '\n\tPrzebiegły Sułtan docisnął parszywego Krzyżowca w piach. \
+    show = print('\n\tPrzebiegły Sułtan docisnął parszywego Krzyżowca w piach. \
         \n\tNastępnie rzucił truchło biedaka swoim niebywale wyrośniętym skorpionom. \
-        Radości nie było końca!\n'
+        Radości nie było końca!\n')
+    return show
 
 
 def show_crusader_wins_ending():
-    return '\n\tKrzyżowiec tryumfuje! Chwała eurpejskiej cywilizacji \
-        \n\troztacza się nad spalonymi poiaskami pustyni. Sułtan gryzie piach.\n'
+    show = print('\n\tKrzyżowiec tryumfuje! Chwała eurpejskiej cywilizacji \
+        \n\troztacza się nad spalonymi poiaskami pustyni. Sułtan gryzie piach.\n')
+    return show
+
 
 def show_tie_ending():
-    return '\n\tI wszystko to jak krew w piach! Dosłownie. Krzyżowiec \
+    show = print('\n\tI wszystko to jak krew w piach! Dosłownie. Krzyżowiec \
         \n\tzagazowany toksycznymi oparami fajki wodnej padł twarzą w wydmę \
         \n\tstając się tym samym pokarmem dla okolicznych grzechotników. \
         \n\tA sam Sułtan jakkolwiek przebiegły, to teraz z nabiegłymi krwią \
         \n\toczyma, przeszedł kilka metrów do swojego wielbłąda zostawiając \
         \n\tza sobą karmazynową ścieżkę hańby. Padł na wznak - zakrzyżowany \
-        \n\tna śmierć.\n'
+        \n\tna śmierć.\n')
+    return show
+
 
 def show_error_illegal_move():
-    return '\n\tTo pole jest już oblegane. Spróbuj Waść ponownie.\n'
+    show = print('\n\tTo pole jest już oblegane. Spróbuj Waść ponownie.\n')
+    return show
+
 
 def show_error_not_valid_type():
-    return '\n\tMoże za dużo gorzałki wczorajszego wieczora... \
-        \nPanie, proszę wprowadzić cyfrę w zakresie 1-9.\n'
+    show = print('\n\tMoże za dużo gorzałki wczorajszego wieczora... \
+        \nPanie, proszę wprowadzić cyfrę w zakresie 1-9.\n')
+    return show
+
 
 def show_not_crusader_error():
-    return '\n\tMoże Krzyżowiec padł zanim stanął... do walki. \
-        \nTrudno, rozgrywka sie nie odbędzie.\n'
+    show = print('\n\tMoże Krzyżowiec padł zanim stanął... do walki. \
+        \nTrudno, rozgrywka sie nie odbędzie.\n')
+    return show
