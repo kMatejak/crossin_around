67d7bd474b35eb8591770fed75b0b70ebbef5208
kMatejak <ksmatejak@gmail.com>
2019-03-04 18:54:37 +0100

    prepared for two languages game versions, added 'tie' cheat in player_moves and clean code

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -1,16 +1,25 @@
 from display_in_console import show
 
 
-def ask_yes_no(question):
+def ask_language_version():
+    pass
+
+
+def ask_yes_no():
     """
     Ask a question that you can answer 'yes' or 'no'
     """
     answer = False
     counter = 0
+    question = show.opening_question()
+
+    show.white_rows(50)
     
-    while answer not in ('t', 'y', 'n', '', 'maciej jest spoko') and counter < 4:
+    while answer not in ['t', 'y', 'maciej jest spoko']:
         answer = input(question).lower()
         counter += 1
+        if counter == 3:
+            raise TypeError
     
     return answer
 
@@ -25,6 +34,7 @@ def ask_number(question):
     while answer not in range(1, 10) and counter < 4:
         answer = int(input(question))
         counter += 1
+
     return answer
 
 
@@ -48,15 +58,23 @@ def create_empty_board():
 
 def player_moves(turn, board):
     EMPTY = ' '
-    move = input('Wskaż pole swojego najazdu: ')
-    if move not in ['1','2','3','4','5','6','7','8','9']:
+    question = show.next_move_ask()
+    field = input(question)
+
+    if field == 'remis':
+        board = {'1':'o','2':'+','3':'o','4':'+','5':'o','6':'+','7':'+','8':'o','9':'+',}
+        return board
+
+    elif field not in ['1','2','3','4','5','6','7','8','9']:
         show.error_not_valid_type()
         raise TypeError
-    elif board[move] != EMPTY:
+
+    elif board[field] != EMPTY:
         show.error_illegal_move()
         raise TypeError
+
     else:
-        board[move] = turn
+        board[field] = turn
         return board
 
 
@@ -89,6 +107,8 @@ def check_winner(symbol, board):
         for el in win_row:
             if board[el] == symbol:
                 check += 1
+                if check == 3:
+                    return symbol
     
     if EMPTY not in board.values():
         return 'tie'
